ce0c0de70d22c052e3b5f1fa695277d8fc4b3c7b
Karsten Zhou <1339688323@qq.com>
2023-02-06 01:10:56 +0800

    迁移[[html:导出数据表格]]

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,114 @@
 <script setup>
 import { ref } from 'vue';
-import darkTheme from 'naive-ui';
+import { darkTheme } from 'naive-ui';
 import sleep from 'await-sleep';
-// import { setLocaleMessage } from 'vue-i18n';
+import * as XLSX from 'xlsx/xlsx.mjs';
+import moment from 'moment';
+
+// 本地测试（开始）
+import axios from 'axios';
+// 本地测试（结束）
+
+// 如果网页链接不是羊羊百科，自动进入测试模式
+let isTesting = location.host === 'xyy.huijiwiki.com' ? false : true;
 
 // 定义一些变量
-let options = ref(null);
-let data = ref(null);
-let msg = ref(null);
-let textLoading = ref(true);
+let options = ref([]);
+let btnLoading = ref(false);
+let selectLoading = ref(false);
+let value = ref(null);
 
-(async function waitCHP() {
-  if ((typeof CHP === 'undefined') || (typeof mw === 'undefined')) {
-    await sleep(3000);
-    waitCHP();
-    console.log('[导出动画数据] 正在等待 CHP 和 Wiki api 就绪');
-  } else {
-    CHP.then(() => {
-      getText();
-    });
-    console.log('[导出动画数据] CHP 和 Wiki api 已就绪');
-  }
-})();
+// 加载配置页面的内容
+async function focus() {
 
-// 加载表格的功能
+  // 如果已经加载过一次了就不再加载
+  if (options.value.length !== 0) { return; }
+
+  // 打开加载动画，强制欣赏加载动画一秒钟，防止加载速度太快出现“闪屏”
+  selectLoading.value = true;
+  await sleep(1000);
+
+  // 本地测试（开始）
+  await sleep(3000);
+  options.value = [
+    {
+      "type": "group",
+      "label": "动画正篇",
+      "key": "动画正篇",
+      "children": [
+        {
+          "label": "嘻哈闯世界",
+          "value": "嘻哈闯世界"
+        },
+        {
+          "label": "羊羊小侦探",
+          "value": "羊羊小侦探"
+        },
+        {
+          "label": "奇幻天空岛",
+          "value": "奇幻天空岛"
+        },
+        {
+          "label": "羊村守护者",
+          "value": "羊村守护者"
+        },
+        {
+          "label": "跨时空救兵",
+          "value": "跨时空救兵"
+        },
+        {
+          "label": "勇闯四季城",
+          "value": "勇闯四季城"
+        }
+      ]
+    },
+    {
+      "type": "group",
+      "label": "动画短剧",
+      "key": "动画短剧",
+      "children": [
+        {
+          "label": "智趣羊学堂之动植物篇",
+          "value": "动植物篇"
+        },
+        {
+          "label": "智趣羊学堂之运动篇",
+          "value": "运动篇"
+        },
+      ]
+    }
+  ];
+  selectLoading.value = false;
+  return;
+  // 本地测试（结束）
+
+  new mw.Api().postWithToken('csrf', {
+    action: 'query',
+    prop: 'revisions',
+    titles: 'Data:导出数据表格.json',
+    rvprop: 'content'
+  }).fail((err) => {
+    $message.error(`未知错误`);
+    console.log(err);
+  }).done((msg) => {
+    let text = msg.query.pages[188].revisions[0]['*'];
+    options.value = (JSON.parse(text))['list'];
+    selectLoading.value = false;
+  });
+
+}
+
+// 导出表格的功能
 async function getXlsx(title) {
   try {
-    this.loading = true;
+    btnLoading.value = true;
     if (title === null) {
-      $message.error(t('blank'));
-      this.loading = false;
+      isTesting ? console.log('请选择需要导出的内容') : $message.error('请选择需要导出的内容');
+      btnLoading.value = false;
     } else {
       let filter = encodeURI(`{"系列1":"${title}"}`);
       let response = await axios.get(`https://xyy.huijiwiki.com/api/rest_v1/namespace/data?filter=${filter}&sort_by=%E9%9B%86%E6%95%B0&pagesize=530&${Date()}`);
-      await $cockpitUtil.sleep(500);
+      await sleep(500);
       await response[`data`][`_embedded`].forEach(element => {
         Object.keys(element).forEach(key => {
           if (typeof element[key] === 'object') {
@@ -46,66 +121,61 @@ async function getXlsx(title) {
       let workbook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(workbook, worksheet, "Data");
       XLSX.writeFile(workbook, `${title}.xlsx`);
-      XLSX.writeFile(workbook, `${title}（时间：${Date()}）（备份：发生意外状况时用这个文件还原）.xlsx`);
-      this.loading = false;
+      XLSX.writeFile(workbook, `${title}（备份：${moment().format('lll')}）.xlsx`);
+      btnLoading.value = false;
     }
   } catch (error) {
-    $message.error(t('unknown'));
+    isTesting ? console.log('未知错误') : $message.error('未知错误');
     console.error(error);
-    this.loading = false;
+    btnLoading.value = false;
   }
 }
-
-// 加载[[Data:导出数据表格.json]]信息的功能
-async function getText() {
-
-  let a = await axios.get('https://xyy.huijiwiki.com/api/rest_v1/namespace/data?filter=%7B%22_id%22%3A%20%22Data%3A%E5%AF%BC%E5%87%BA%E6%95%B0%E6%8D%AE%E8%A1%A8%E6%A0%BC.json%22%7D');
-  a = a['data']['_embedded'][0];
-  options.value = a['list'];
-
-  // 处理多语言
-  Object.keys(a['messages']).forEach(element => {
-    setLocaleMessage(element, a['messages'][element]);
-  });
-
-  let locale = mw.config.values.wgUserLanguage;
-
-  // 结束加载状态
-  textLoading.value = false;
-
-};
 
 </script>
 
 <template>
+
+  <!-- 添加 Material Symbol 系列图标 -->
+  <!-- 本地测试（开始） -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,300,0,0" />
+  <!-- 本地测试（结束） -->
+
   <n-config-provider :theme="darkTheme">
+    <n-card title="导出数据表格">
 
-    <!-- 加载中展示的卡片 -->
-    <n-card v-if="textLoading">
-      <n-skeleton text :repeat="5" /> <n-skeleton text style="width: 60%" />
-    </n-card>
+      <n-p>在此处选择动画系列即可导出剧集数据表格，方便对内容进行批量修改。修改后的表格需要使用“灰机Wiki数据更新器”上传，才能更新羊羊百科的数据。</n-p>
 
-    <!-- 加载完成后展示的卡片 -->
-    <n-card v-else :title="t(`title`)">
-      <n-p v-html="t('read_me')"></n-p>
-      <template #footer>
-        <n-a href="https://github.com/XYY-huijiwiki/XYY-huijiwiki-components" target="_blank">
-          {{ t('github_link') }}
-        </n-a>
-        &nbsp;|&nbsp;
-        <n-a href="//xyy.huijiwiki.com/wiki/Data:导出数据表格.json" target="_blank">{{ t('conf_page_link') }}</n-a>
-        &nbsp;|&nbsp;
-        <n-a href="//xyy.huijiwiki.com/wiki/Html:导出数据表格" target="_blank">{{ t('html_page_link') }}</n-a>
-        &nbsp;|&nbsp;
-        <n-a href="//www.huijiwiki.com/wiki/帮助:灰机Wiki数据更新器" target="_blank">{{ t('huiji_updater_link') }}</n-a>
-      </template>
+      <n-input-group>
+        <n-select v-model:value="value" :options="options" :loading="selectLoading" @focus="focus" filterable>
+          <template #empty>
+            <n-empty description="正在加载">
+              <template #icon>
+                <n-icon>
+                  <material-symbol :size="32">hourglass_empty</material-symbol>
+                </n-icon>
+              </template>
+            </n-empty>
+          </template>
+        </n-select>
+        <n-button @click="getXlsx(value)" :loading="btnLoading">导出</n-button>
+      </n-input-group>
+
+      <!-- 底部footer -->
       <template #action>
-        <n-input-group>
-          <n-select v-model:value="value" :options="options" :placeholder="t(`placeholder`)" filterable></n-select>
-          <n-button @click="getXlsx(value)" :loading="loading">{{ t(`btn_in`) }}</n-button>
-        </n-input-group>
+        <n-space justify="space-between">
+          <!-- 本地测试（开始） -->
+          <n-p>当前为本地测试</n-p>
+          <!-- 本地测试（结束） -->
+          <n-p>最后编译时间：Feb 6, 2023 1:09 AM</n-p>
+          <n-space justify="end">
+            <n-a href="//github.com/XYY-huijiwiki/XYY-huijiwiki-components" target="_blank">Github</n-a>
+            <n-a href="//xyy.huijiwiki.com/wiki/Data:导出数据表格.json" target="_blank">配置页面</n-a>
+            <n-a href="//www.huijiwiki.com/wiki/帮助:灰机Wiki数据更新器" target="_blank">灰机Wiki数据更新器</n-a>
+          </n-space>
+        </n-space>
       </template>
-    </n-card>
 
+    </n-card>
   </n-config-provider>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createI18n } from 'vue-i18n';
 
-// 配置国际化插件
-const i18n = createI18n({
-    legacy: false,
-    fallbackLocale: 'zh',
-});
-
-let app = createApp(App);
-app.use(i18n);
-app.mount('#cartoon-data-output');
+createApp(App).mount('#cartoon-data-output');
